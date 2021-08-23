c55a308b5f6ee55366f3915ffb7da24be115b3d6
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-08-23 15:34:16 -0500

    completed sugarsyntax task

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            var answer = int.Parse(Console.ReadLine());
+            var response = (answer < 9) ? $"{answer} is less than nine." : $"{answer} greater than or equal to nine.";
+
+            Console.WriteLine(response);
+        }
+    }
+}
